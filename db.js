6e69c252de544ba7c28b3dc1054bf2a8d7edf56c
Abdulsalam <abdulsalamadigun19@gmail.com>
2025-11-14 10:51:10 +0100

    Deploy to Render

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
 // db.js
 const { Pool } = require('pg');
-require('dotenv').config(); // ← This loads .env file
+require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false  // Required for Render
+  }
 });
 
 module.exports = pool;
+
+// // db.js
+// const { Pool } = require('pg');
+// require('dotenv').config(); // ← This loads .env file
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+// });
+
+// module.exports = pool;
