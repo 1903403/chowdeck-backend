01049e040d9b73830b5673c4e005e7a48170b7e8
Abdulsalam <abdulsalamadigun19@gmail.com>
2025-11-14 10:53:38 +0100

    Deploy to Render

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const Intl = require('intl');
 require('intl/locale-data/jsonp/en');
 
 const app = express();
+// const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Middleware
 app.use(cors({origin: '*'}));  // Allows Flutter calls
